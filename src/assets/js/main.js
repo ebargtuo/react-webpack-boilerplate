76f685fce2052e203cbd7c87942218811ab33640
ebargtuo <ebargtuo@gmx.it>
2015-04-12 14:14:56 +0200

    Change html element class when javascript is available

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,8 @@
 (function() {
     "use strict";
 
+    document.getElementsByTagName("html")[0].className = "js";
+
     var h1 = document.getElementsByTagName("h1")[0];
     h1.style.backgroundColor = "lightblue";
 })();
